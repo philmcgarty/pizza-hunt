26a5cf7ebf8c0d54b09d12003bca9a0c4b953435
Phil McGarty <philmcgarty@hotmail.com>
2022-06-13 23:21:18 -0400

    added comment model and routes

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -6,6 +6,12 @@ const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 })
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -16,6 +22,11 @@ const pizzaController = {
     // get 1 pizza by id
     getPizzaById({ params }, res) {
         Pizza.findOne({ _id: params.id })
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
             .then(dbPizzaData => {
                 // if not found
                 if (!dbPizzaData) {
